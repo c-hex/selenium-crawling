8dc619af2d23548e68195b9f7fb0a409dc96c557
류채연 <haha03270@gmail.com>
2023-05-16 16:02:29 +0900

    add

---
+++ danawa.py
@@ -0,0 +1,60 @@
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome import options
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common import NoSuchElementException
+import pymysql
+
+db = pymysql.connect(host='localhost', port=3306, user='root', password='1234')
+cursor = db.cursor()
+sql = "insert into crawler.danawacrawler(name,price,`option`) values (%s, %s, %s);"
+
+def main():
+    option = options.Options()
+    option.add_experimental_option('detach', True)
+    service = ChromeService(executable_path=ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service,
+                              options=option)
+    driver.get("https://www.danawa.com/")
+    driver.find_element(By.ID, "AKCSearch").send_keys("노트북"+Keys.ENTER)
+    time.sleep(2)
+    ########
+    for pageNo in range(3):
+        driver.execute_script("getPage("+str(pageNo+1)+")")
+        time.sleep(2)
+        products = driver.find_element(By.CLASS_NAME, "main_prodlist_list") \
+            .find_elements(By.CLASS_NAME, "prod_item")
+        for item in products:
+            if item.get_attribute("id") != "":
+                # print(item.get_attribute("id"))
+                name = item.find_element(By.CLASS_NAME, "prod_name").text
+                try:
+                    item \
+                        .find_element(By.CLASS_NAME, "more_prod_pricelist") \
+                        .find_element(By.TAG_NAME, "a").click()
+
+                except NoSuchElementException:
+                    pass
+                prices = item. \
+                    find_element(By.CLASS_NAME, "prod_pricelist") \
+                    .find_elements(By.TAG_NAME, "li")
+                for item in prices:
+                    price = item \
+                        .find_element(By.CLASS_NAME, "price_sect") \
+                        .find_element(By.TAG_NAME, "a")
+                    option = item.find_element(By.CLASS_NAME,
+                                               "memory_sect").text
+                    if '1위' in option:
+                        option = option.replace("1위 ", "")
+                    if '2위' in option:
+                        option = option.replace("2위 ", "")
+                    cursor.execute(sql, (name, price.text, option))
+                    db.commit()
+                    print(name, price.text, option)
+
+if __name__ == '__main__':
+    main()
---
+++ kreamcrawling.py
@@ -0,0 +1,91 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome import options
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pymysql
+db = pymysql.connect(host='localhost', port=3306,
+                     user='root', password='1234')
+cursor = db.cursor()
+insertSQL = "INSERT INTO crawler.kream_url (URL, product_id) VALUES(%s, %s);"
+
+option = options.Options()
+option.add_experimental_option('detach', True)
+option.add_argument("--window-size=1920,1080")
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service,
+                          options=option)
+def getData():
+    selectSQL = "SELECT URL, product_id, flag FROM " \
+                "crawler.kream_url where flag = 'N';"
+    updateFlagSQL = "UPDATE crawler.kream_url SET flag='Y' WHERE product_id=%s;"
+    insertProductSQL = "INSERT INTO crawler.kream_data " \
+                       "(productId, brand, name, subName, price, url, releaseDate, color, originPrice, modelNum) " \
+                       "VALUES(%s, %s, %s,%s, %s, %s, %s, %s, %s, %s);"
+    cursor.execute(selectSQL)
+    rows = cursor.fetchall()
+    for i in rows:
+        driver.get(i[0])
+        time.sleep(3)
+        box = driver.find_element(By.CLASS_NAME, "main_title_box")
+        name = box.find_element(By.CLASS_NAME, "title").text
+        print(name)
+        image = driver.find_element(By.CLASS_NAME, "item_inner") \
+            .find_element(By.TAG_NAME, "img").get_attribute("src")
+        if image == "https://kream.co.kr/images/common_thumbs_blank_L.png?type=l":
+            driver.refresh()
+            continue;
+
+        brand = box.find_element(By.CLASS_NAME, "brand").text
+        subname = box.find_element(By.CLASS_NAME, "sub_title").text
+        priceBox = driver.find_element(By.CLASS_NAME, "detail_price")
+        price = priceBox.find_element(By.CLASS_NAME, "num").text
+        # print(brand,name,subname,price)
+        detailBox = driver.find_elements(By.CLASS_NAME, 'detail_box')
+        modelNum = detailBox[0].find_element(By.CLASS_NAME, "product_info").text
+        releaseDate = detailBox[1].find_element(By.CLASS_NAME, "product_info").text
+        color = detailBox[2].find_element(By.CLASS_NAME, "product_info").text
+        originPrice = detailBox[3].find_element(By.CLASS_NAME, "product_info").text
+        cursor.execute(insertProductSQL, (i[1], brand, name,
+                                         subname, price, image,
+                                         releaseDate, color,
+                                         originPrice, modelNum))
+        cursor.execute(updateFlagSQL, (i[1]))
+        db.commit()
+
+        # for detail in detailBox:
+        #     title = detail.find_element(By.CLASS_NAME, "product_title")
+        #     info = detail.find_element(By.CLASS_NAME, "product_info")
+        #     print(title.text, info.text)
+
+
+def getURL():
+    driver.get("https://kream.co.kr/search?tab=44&keyword=에어포스")
+
+    total = driver.find_element(By.CLASS_NAME,
+                                "filter_result").text
+    total = int(total.replace("상품", "")
+                .replace(",", "").strip())
+    total = 500
+    count = 0
+    while count < total:
+        driver.execute_script("window.scrollTo"
+                          "(0,document.body.scrollHeight)")
+        time.sleep(1)
+        itemList = driver.find_elements(By.CLASS_NAME, "product_card")
+        count = len(itemList)
+    for item in itemList:
+        href = item.find_element(By.TAG_NAME, "a")\
+            .get_attribute("href")
+        print(href)
+        id = href.split("/")[4]
+        print(id)
+        cursor.execute(insertSQL, (href, id))
+        db.commit()
+
+def main():
+    getData()
+if __name__ == '__main__':
+    main()
